ac4ea8a160b922f980ccf59e654382e51f82c782
ROHIT VISHNU GHUMARE <48523873+rohitg00@users.noreply.github.com>
2020-08-13 00:39:03 +0530

    Update devopstask.groovy

--- devopstask.groovy
+++ devopstask.groovy
@@ -20,7 +20,7 @@ description ("It will run the containers")
    steps {
         shell('''cd /srv/djangoapp
 sudo docker pull rohitghumare64/devopsblog:v1
-sudo docker run -it --name c1 --privileged -p 1234:8080  devopsblog:v1
+sudo docker run -dit --name c1 --privileged -p 1234:8080  devopsblog:v1
 if sudo docker ps | c1
 then
 sudo docker rm -f devopsblog
